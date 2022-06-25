1a50f1dbd4d5f3c671f4210d47875f06af088542
Saul <saul.navarro.utj@gmail.com>
2022-06-25 00:59:14 -0500

    done

---
+++ server.js
@@ -0,0 +1,35 @@
+const Contenedor = require('./src/clases/Contenedor');
+const products = new Contenedor('./src/data/productos.txt');
+(async () => {
+    await products.deleteAll();
+    await products.save({title: 'Node js', price: 10, thumbnail:'https://i0.wp.com/www.jacobsoft.com.mx/wp-content/uploads/2020/04/node-js-736399_960_720-2.png?fit=960%2C480&ssl=1'});
+    await products.save({title: 'Express js', price: 10, thumbnail:'https://i0.wp.com/www.jacobsoft.com.mx/wp-content/uploads/2020/04/node-js-736399_960_720-2.png?fit=960%2C480&ssl=1'});
+    await products.save({title: 'Glitch', price: 10, thumbnail:'https://i0.wp.com/www.jacobsoft.com.mx/wp-content/uploads/2020/04/node-js-736399_960_720-2.png?fit=960%2C480&ssl=1'});
+})();
+
+
+const app = require('express')();
+const PORT = process.env.PORT || 8080;
+
+app.get('/products', async (req, res) => {
+    try {
+        res.status(200).send(await products.getAll());
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.get('/randomProduct', async (req, res) => {
+    try {
+        let randomNumber = Math.floor(Math.random() * 3 +1);
+        res.json(await products.getById(randomNumber));
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+});
+
+server.on('error', (error) => console.error(error));
---
+++ src/clases/Contenedor.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+
+class Contenedor {
+    constructor(filename) {
+        this.filename = filename;
+    }
+
+    async save(product) {
+        try {
+            let content = await this.readFile();
+            product.id = this.buildId(content);
+            
+            content.push(product);
+            
+            await this.writeFile(content);
+
+            return product.id;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async getById(id) {
+        try {
+            let content = await this.readFile();
+            content = content.filter((product) => product.id === id);
+            return content.length == 0 ? null : content;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
+    getAll() {
+        try {
+            return this.readFile();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
+    async deleteById(id) {
+        try {
+            let content = await this.readFile();
+            this.writeFile(content.filter((product) => product.id !== id));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
+    async deleteAll() {
+        try {
+            this.writeFile([]);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
+    async readFile() {
+        try {
+            return JSON.parse(await fs.promises.readFile(this.filename, 'utf-8'));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
+    async writeFile(content) {
+        try {
+            await fs.promises.writeFile(this.filename, JSON.stringify(content));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    buildId(content) {
+        try {
+            if (content.length === 0) {
+                return 1;
+            } else {
+                content.sort((a, b) => (a.id > b.id ? 1 : -1));
+                return content[content.length - 1].id + 1;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+module.exports = Contenedor;
